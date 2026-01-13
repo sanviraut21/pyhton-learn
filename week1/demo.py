2d6459a308fac96c02d31d806a486828883dcfb4
Arham <rudra298.rj@gmail.com>
2026-01-13 13:43:40 +0530

    odd even commit

--- week1/demo.py
+++ week1/demo.py
@@ -1,9 +1,15 @@
-def greet():
-    print("Hi!!  Hello Pookies")
-    
-    def main():
-        greet()
-        
-        if _name_=="_main_":
-            main()
-    
+def get_factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * get_factorial(n-1)
+def getNumber():
+    return int(input("Enter a number:  "))
+def main():
+    num=getNumber()
+    fact = get_factorial(num)
+    print(f"Factorial of {num} is {fact}")
+            
+if __name__=="__main__":
+    main()
+                
